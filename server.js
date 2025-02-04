903094574817246ff8df2838d5805dfdf469ef8c
Jimy <jimybombela@yahoo.com>
2025-02-03 20:29:19 -0800

    upgraded websockets to wss

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
+const fs = require('fs');
+const https = require('https')
 const WebSocket = require('ws');
-const server = new WebSocket.Server({ port: 5000 });
 
-server.on('connection', (ws) => {
+const server = https.createServer({
+    cert: fs.readFileSync('cert.pem'),
+    key: fs.readFileSync('key.pem')
+});
+
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
     console.log('Client connected');
 
     ws.on('message', (message) => {
@@ -12,4 +20,6 @@ server.on('connection', (ws) => {
     ws.on('close', () => console.log('Client disconnected'));
 });
 
-console.log("WebSocket server running on ws://localhost:5000");
+server.listen(5000, () => {
+    console.log("Secure WebSocket server running on wss://localhost:5000");
+});
