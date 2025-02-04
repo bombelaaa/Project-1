43c01d787478e094e0f70503bc160bd6cec97d83
Jimy <jimybombela@yahoo.com>
2025-02-03 19:51:36 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const WebSocket = require('ws');
+const server = new WebSocket.Server({ port: 5000 });
+
+server.on('connection', (ws) => {
+    console.log('Client connected');
+
+    ws.on('message', (message) => {
+        console.log(`Received: ${message}`);
+        ws.send(`Echo: ${message}`);
+    });
+
+    ws.on('close', () => console.log('Client disconnected'));
+});
+
+console.log("WebSocket server running on ws://localhost:5000");
